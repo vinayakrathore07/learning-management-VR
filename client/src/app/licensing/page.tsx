0956f418aae72cc6d788e360fa3853380d013ec8
vinayakrathore07 <vinayakrathore007@gmail.com>
2025-12-28 21:46:27 +0530

    Fix footer links and add placeholder pages

---
+++ client/src/app/licensing/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Coming Soon</h1>
+      <p>This page will be updated.</p>
+    </div>
+  );
+}
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,24 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Footer() {
   return (
-    <div className='footer'>
-        <p>&copy; © 2025 Vinayak LMS. All rights reserved.</p>
-        <div className='footer__links'>
-           {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
-          <Link 
+    <div className="footer">
+      <p>&copy; 2025 Vinayak LMS. All rights reserved.</p>
+
+      <div className="footer__links">
+        {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
+          <Link
             key={item}
             href={`/${item.toLowerCase().replace(" ", "-")}`}
-             className="footer__link">
-              scroll={false}
-              
-                {item}
-             </Link>
-           ))}
-        </div>
+            scroll={false}
+            className="footer__link"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
